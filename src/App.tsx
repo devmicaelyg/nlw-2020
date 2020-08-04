8fcfcf8a66bbb0c754f16d684ab01a7ec6916382
Micaely Gusmão <devmicaelyg@gmail.com>
2020-08-03 23:09:00 -0300

    Detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './assets/style/global.css'
+import Landing from './pages/Landing/Landing';
 
 function App() {
   return (
-    <div className="App">
+      <Landing />
 
-    </div>
   );
 }
 
