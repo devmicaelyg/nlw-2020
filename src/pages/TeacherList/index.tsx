055109995605d598699ca24f6cf00bb82e48193c
Micaely Gusmão <devmicaelyg@gmail.com>
2020-08-05 21:37:00 -0300

    Componentizando o TeachItem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
+
 
 const TeacherList = () => {
     return(
@@ -28,33 +29,11 @@ const TeacherList = () => {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars2.githubusercontent.com/u/65607825?s=460&u=bca58e2317ab740ffdd7d00d82c4d97fb12103c3&v=4" alt="Micaely Gusmão" />
-                        <div>
-                            <strong> Micaely Gusmão</strong>
-                            <span> JavaScript </span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Estudante de análise e desenvolvimento de sistemas.
-
-                        <br /> <br />
-                        Apaixonada por programação e por ensinar. Amante de futebol e música. 
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong> R$ 50,00 </strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="whatsapp" />
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
        </div>
     );
